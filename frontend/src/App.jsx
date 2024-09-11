ab3eed6bf321daabfe2d37cbdb5362a7eda8cee3
letItBezhumphries <ezhumphries@gmail.com>
2024-09-11 13:45:57 -0700

    moved all the screens into the main jsx file and restructured the react router dom to incorporate the privateroute component for protecting auth routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,40 @@
 import './App.css';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { Routes, Route } from 'react-router-dom';
+import { logout } from './slices/authSlice';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import HomeScreen from './screens/HomeScreen';
-import MonstersScreen from './screens/MonstersScreen/MonstersScreen';
-import CharacterScreen from './screens/CharacterBuilderScreens/CharacterScreen';
-import ChooseRaceScreen from './screens/CharacterBuilderScreens/ChooseRace/ChooseRaceScreen';
-import ChooseClassScreen from './screens/CharacterBuilderScreens/ChooseClass/ChooseClassScreen';
-import ChooseAbilitiesScreen from './screens/CharacterBuilderScreens/ChooseAbilities/ChooseAbilitiesScreen';
-import ProfileScreen from './screens/ProfileScreens/ProfileScreen';
-import CharacterDetailsScreen from './screens/CharacterDetailsScreen/CharacterDetails';
-import CharacterEditScreen from './screens/admin/CharacterEditScreen';
-import CharacterListScreen from './screens/admin/CharacterListScreen';
-// import UserListScreen from './screens/admin/UserListScreen';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (expirationTime) {
+      const currentTime = new Date().getTime();
+
+      if (currentTime > expirationTime) {
+        dispatch(logout());
+      }
+    }
+  }, [dispatch]);
+
   return (
     <div id='app'>
-      <Navbar />
-      <div className='main'>
-        <Routes>
-          <Route path='/' element={<HomeScreen />} exact></Route>
-          <Route path='/monsters' element={<MonstersScreen />} exact></Route>
-          <Route path='/character' element={<CharacterScreen />}></Route>
-          <Route
-            path='/character/chrace'
-            element={<ChooseRaceScreen />}
-            exact
-          ></Route>
-          <Route
-            path='/character/chclass'
-            element={<ChooseClassScreen />}
-            exact
-          ></Route>
-          <Route
-            path='/character/chabilities'
-            element={<ChooseAbilitiesScreen />}
-            exact
-          ></Route>
-          <Route
-            path='/character/:id'
-            element={<CharacterDetailsScreen />}
-            exact
-          ></Route>
-          <Route path='/profile' element={<ProfileScreen />} exact></Route>
-          <Route
-            path='/profile/character/:id/edit'
-            element={<CharacterEditScreen />}
-            exact
-          ></Route>
-          <Route path='/login' element={<LoginScreen />} exact></Route>
-          <Route path='/register' element={<RegisterScreen />} exact></Route>
-        </Routes>
-      </div>
-      <Footer />
       <ToastContainer />
+      <Navbar />
+      <main className='main'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,67 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './bootstrap.min.css';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import PrivateRoute from './components/PrivateRoute';
+import HomeScreen from './screens/HomeScreen';
+import MonstersScreen from './screens/MonstersScreen/MonstersScreen';
+import CharacterScreen from './screens/CharacterBuilderScreens/CharacterScreen';
+import ChooseRaceScreen from './screens/CharacterBuilderScreens/ChooseRace/ChooseRaceScreen';
+import ChooseClassScreen from './screens/CharacterBuilderScreens/ChooseClass/ChooseClassScreen';
+import ChooseAbilitiesScreen from './screens/CharacterBuilderScreens/ChooseAbilities/ChooseAbilitiesScreen';
+import ProfileScreen from './screens/ProfileScreens/ProfileScreen';
+import CharacterDetailsScreen from './screens/CharacterDetailsScreen/CharacterDetails';
+import CharacterEditScreen from './screens/admin/CharacterEditScreen';
+import CharacterListScreen from './screens/admin/CharacterListScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/monsters' element={<MonstersScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+      {/* Registered users */}
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/character' element={<CharacterScreen />}></Route>
+        <Route
+          path='/character/chrace'
+          element={<ChooseRaceScreen />}
+          exact
+        ></Route>
+        <Route
+          path='/character/chclass'
+          element={<ChooseClassScreen />}
+          exact
+        ></Route>
+        <Route
+          path='/character/chabilities'
+          element={<ChooseAbilitiesScreen />}
+          exact
+        ></Route>
+        <Route
+          path='/character/:id'
+          element={<CharacterDetailsScreen />}
+          exact
+        ></Route>
+      </Route>
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
